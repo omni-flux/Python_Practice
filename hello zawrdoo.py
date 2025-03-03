9b858b90e2007c198e14722bb5a5cea677916e49
kentuckyfriedkim-chi <ommkarsos@gmail.com>
2025-03-03 11:56:21 +0530

    started course

--- hello zawrdoo.py
+++ hello zawrdoo.py
@@ -1,6 +1,116 @@
-import torch
-print(torch.cuda.is_available())
+"""
+starting my udemy course python date 3/3/2025
 
+some usefull pycharm shortcuts
+   shift + F10 -> run file
+   shift + Alt + , or . -> increase or decrease font size
+   ctrl + d -> duplicate the line
+   ctrl + alt + l -> short-cut for formating
+"""
+# print("hello")
+# python is case-sensitive so ✔️print()  and Print()❌
+# print("hello"+" bob")
 
+# variables and constants
+# imvar = 'Variable'
+# IMCONST = 'Constant'
+# python has no true constant so
+# you could still change the value of a constant
 
+# Data type
+
+# imaginary = 9j
+# print(type(imaginary))
+
+# Nummeric type
+# number = -100
+# percent = 1.50
+# imaginary = 9j
+
+# Bool types
+# bool = True
+# bool = False
+
+# Sequence types
+# numbers = [1,2,3] list
+# coordinates = (2.5,1.0) tuple
+
+# mapping types
+# dictionary = {'Mario': 1,'Luigi': 2}
+
+# set types
+# raffel = {1,2,3,4} can change
+# frozenraffel = frozenset({1,2,3,4}) cant change
+
+# Type hints / anotations only for dev python dont care
+# text : str  = '10' ✔️
+# text : str = 10 will  give warning but python will still coppile
+
+# Integer
+# age: int = 21
+# money: int = 0
+# self_esteem: int = -100
+
+# Float
+# PI: float = 3.14
+
+# Operators
+
+# + - / *
+# floor division //
+# exponentioal **
+# modulus %
+
+# x: int = 2
+# x += 2 # x = x + 2
+# += -= *= **= /= //= %=
+
+# comparision
+# a == b
+# a != b
+# a > b
+# a < b
+# a >= b
+# a <= b
+# a > d > c
+
+# logical
+#  a == b and a == b
+#  or
+#  not((a==b))
+
+# string
+# name : str = 'omkar'
+# name : str = "omkar"
+#say i want to adress some thing thats mine
+# name : str = "omkar's"  python be like :😊
+# name : str = 'omkar's' python be like :😠
+# so
+# name : str = 'omkar\'s' python be like :😊👍
+
+# quote : str = "he said:"hello"" python be like :😒👎
+# quote : str = "he said:\"hello\"" python be like :😊👍
+#
+# str concatination
+# print(name +" "+ quote)
+
+# poem : str = """
+# dhsbhd
+# sdcshb
+# sdcnjsk
+# cdjk
+# """
+
+# type conversion
+# txt_value = '100'
+# int_value = 50
+#
+# print(txt_value + str(int_value) ) -> 10050
+#
+# also if trying to convert incompatible
+# values we get value error for exp
+#
+# print(int('ten')) -> value error
+
+# - made simple adder project
 
